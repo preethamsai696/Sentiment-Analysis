fccc04e165c94d4fba2fc85c7f45ab5ba956c0cd
preethamsai696 <69846234+preethamsai696@users.noreply.github.com>
2020-09-03 15:08:10 +0530

    Update IMDB-App.py

--- IMDB-App.py
+++ IMDB-App.py
@@ -12,13 +12,14 @@ st.write(" Accuracy: 0.87 ")
 
 data = pd.read_csv('https://drive.google.com/file/d/1SJPMM11AcA9D0fudo_VpzX9P8NxwYqP9/view?usp=sharing.csv',header=None,error_bad_lines=False)
 
+x2 = data.iloc[:,0]
 x1 = data.iloc[:,0].values
 y1  =data.iloc[:,1].values
 
 sentence = st.text_input(" Write your review here : ")
 
 v = TfidfVectorizer(decode_error='replace', encoding='utf-8')
-x1 =  v.fit_transform(x1.values.astype('U'))
+x1 =  v.fit_transform(x2.values.astype('U'))
 
 model1 = MultinomialNB()
 model.fit(x1,y1)
